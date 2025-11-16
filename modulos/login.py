6da7c62104554b9bf45406d1825beefe85944e1b
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-15 20:18:47 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from modulos.Configuracion.conexion import obtener_conexion
+from modulos.Configuración.conexion import obtener_conexion
+
 
 def login():
     st.title("🔐 Iniciar sesión")
