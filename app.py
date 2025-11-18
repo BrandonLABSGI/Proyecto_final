d3bc52a74d6c1ba0b872f22738cd81b358575f32
BrandonLABSGI <00202521@uca.edu.sv>
2025-11-18 12:56:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 import streamlit as st
 from modulos.login import login
 from modulos.directiva import interfaz_directiva
+from modulos.promotora import interfaz_promotora
 
-# ---------------------------------------------------
-# ESTADO DE SESIÓN PARA CONTROLAR LOGIN / LOGOUT
-# ---------------------------------------------------
+# ----------------------------------------------
+# ESTADO DE SESIÓN PARA LOGIN
+# ----------------------------------------------
 
 if "sesion_iniciada" not in st.session_state:
     st.session_state["sesion_iniciada"] = False
 
-# ---------------------------------------------------
-# MOSTRAR PANTALLA SEGÚN SESIÓN
-# ---------------------------------------------------
+if "rol" not in st.session_state:
+    st.session_state["rol"] = None
+
+# ----------------------------------------------
+# MOSTRAR PANTALLA SEGÚN ROL
+# ----------------------------------------------
 
 if st.session_state["sesion_iniciada"]:
-    # Si la sesión está activa -> cargar menú de directiva
-    interfaz_directiva()
+
+    if st.session_state["rol"] == "directiva":
+        interfaz_directiva()
+
+    elif st.session_state["rol"] == "promotora":
+        interfaz_promotora()
+
+    else:
+        st.error("Rol no reconocido.")
+
 else:
-    # Si no ha iniciado sesión -> mostrar login
     login()
