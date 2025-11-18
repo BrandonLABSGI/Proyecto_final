275f3c041b31bcac7f1b75545a47e5bdf1782b20
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-17 20:40:42 -0600

    Update venta.py

--- modulos/venta.py
+++ modulos/venta.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from modulos.config.conexion import obtener_conexion
+from modulos.conexion import obtener_conexion
+
 
 # -------------------------------------------------------------
 # 🛒 Módulo de Registro de Ventas
