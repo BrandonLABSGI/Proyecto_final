f6a8ec5508a7fc8d8d81aa65f47f926db5ae0acf
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-17 22:16:02 -0600

    Update conexion.py

--- modulos/conexion.py
+++ modulos/conexion.py
@@ -7,13 +7,12 @@ def obtener_conexion():
             host="btcfcbzptdyxq4f8afmu-mysql.services.clever-cloud.com",
             user="unruixx62rfqfqi5",
             password="tHsn5wIjxSzedGOsZmtL",
-            database="btcfcbzptdyxq4f8afmu",
-            port=3306
+            database="btcfcbzptdyxq4f8afmu"
         )
-
     except Exception as e:
         st.error(f"Error al conectar con la base de datos: {e}")
         return None
+
 
 
 
