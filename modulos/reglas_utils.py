7897cb0146c05291bd4748f34e3bea9d25ea4e29
BrandonLABSGI <00202521@uca.edu.sv>
2025-11-22 14:57:23 -0600

    Update reglas_utils.py

--- modulos/reglas_utils.py
+++ modulos/reglas_utils.py
@@ -1,13 +1,18 @@
+import mysql.connector
 from modulos.conexion import obtener_conexion
 
 def obtener_reglas():
+    """
+    Devuelve un diccionario con todas las reglas internas existentes.
+    Siempre toma la primera fila porque la tabla solo guarda una regla activa.
+    """
     con = obtener_conexion()
     cursor = con.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM reglas_grupo ORDER BY id_regla DESC LIMIT 1")
+    cursor.execute("SELECT * FROM reglas_grupo LIMIT 1")
     reglas = cursor.fetchone()
 
     cursor.close()
     con.close()
-
     return reglas
+
